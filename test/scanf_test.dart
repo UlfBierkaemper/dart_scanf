7f1d83463f9cbfdf3fbcfb66900883fb8e407261
Ulf Bierkämper <ulf.bierkaemper@googlemail.com>
2024-08-09 11:46:44 +0200

    Typo

--- test/scanf_test.dart
+++ test/scanf_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:scanf/scanf.dart';
 
 void main() {
-  test('Scanf match tests', () {
+  test('ScanF match tests', () {
     final scanf = ScanF('Pi: %f, Int=%i, Octal=%o %[^:]:%f%%, %[a-c]%s');
     final result = scanf.match('Pi: 3.14, Int=0xCAFEBABE, Octal=755 Progress:34.2%, abcdef');
     expect(result, [3.14, 3405691582, 493, 'Progress', 34.2, 'abc', 'def']);
