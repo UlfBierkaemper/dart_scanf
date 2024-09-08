83847dbcd3da998c2c53300aed2cd02d0a041fdd
Ulf Bierkämper <ulf.bierkaemper@googlemail.com>
2024-09-09 00:23:49 +0200

    Code formatted

--- test/scanf_test.dart
+++ test/scanf_test.dart
@@ -3,9 +3,21 @@ import 'package:test/test.dart';
 
 void main() {
   test('ScanF match tests', () {
-    final scanf = ScanF('Real: %e, Value: %e, Float: %f, Int=%i, Octal=%o %[^:]:%f%%, %[a-c]%s');
-    final result = scanf.match('Real: -3.14E+2, Value: -3.14E-2, Float: 3.14, Int=0xCAFEBABE, Octal=755 Progress:34.2%, abcdef');
+    final scanf = ScanF(
+        'Real: %e, Value: %e, Float: %f, Int=%i, Octal=%o %[^:]:%f%%, %[a-c]%s');
+    final result = scanf.match(
+        'Real: -3.14E+2, Value: -3.14E-2, Float: 3.14, Int=0xCAFEBABE, Octal=755 Progress:34.2%, abcdef');
     print(scanf);
-    expect(result, [-314.0, -0.0314, 3.14, 3405691582, 493, 'Progress', 34.2, 'abc', 'def']);
+    expect(result, [
+      -314.0,
+      -0.0314,
+      3.14,
+      3405691582,
+      493,
+      'Progress',
+      34.2,
+      'abc',
+      'def'
+    ]);
   });
 }
